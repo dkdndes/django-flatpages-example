ca58e1b040800f4d7f306a894188c6bb47b29464
Peter <dkdndes@gmail.com>
2014-03-17 15:32:20 +0100

    added url for flatpage

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
 
+# Setting for STATIC_URL i developement environment
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Django Admin
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,10 +14,12 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    # Tinymce requires access to static files
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# Add an entry in your URLconf
-urlpatterns += patterns('',
-      (r'^pages/', include('django.contrib.flatpages.urls')),
-    #
+
+# djangoproject.com documentation, works with /pages ...
+# (r'^pages/', include('django.contrib.flatpages.urls')),
+urlpatterns += patterns('django.contrib.flatpages.views',
+    (r'^(?P<url>.*/)$', 'flatpage'),
 )
